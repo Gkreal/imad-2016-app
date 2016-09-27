ac955cb15e60d4b4778b45c754e88a0bf530bfba
Gkreal <garimakhanna.k@gmail.com>
2016-09-27 17:21:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,16 +5,34 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ 
 });
+
+
+aap.get('/article-one', function(req, res){
+    res.send('Article one will be served here');
+});
+
+aap.get('/article-two', function(req, res){
+    res.send('Article 2 will be served here');
+});
+
+aap.get('/article-three', function(req, res){
+    res.send('Article 3 will be served here');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'bgg.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
