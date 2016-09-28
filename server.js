708ccaaaf4b096e6cddcec7a318db9bef98a9dc1
Gkreal <garimakhanna.k@gmail.com>
2016-09-28 22:30:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function( req, res){
     //articlename= Article-one
-        var articleName= req.params.articleName;
+        var articleName = req.params.articleName;
         res.send(createTemplate(articles(articleName)));
 });
 
