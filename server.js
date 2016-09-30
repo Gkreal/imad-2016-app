c7d6ad999a90d6f8b48a9e37f1fe0642cb803555
Gkreal <garimakhanna.k@gmail.com>
2016-09-30 16:13:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var articles = {
         content:`<p>
         <br>
         <img src= "http://www.frog-clip-art.com/free_frog_clipart/cartoon_frog_waving_0521-1101-1912-4822_SMU.jpg"> 
-        <br>'This is my friend bob'; <img src="http://www.gifs.net/Animation11/Creatures_and_Cartoons/Cartoon_Characters/Blue_thing.gif">
+        <br>'This is my friend bob'; <img src="http://www.frog-clip-art.com/free_frog_clipart/clip_art_illustration_of_a_happy_frog_wearing_a_top_hat_and_bow_tie_sitting_on_a_lily_pad_0521-1102-0812-5006_SMU.jpg">
                     <li>
                 
                 </p>`
