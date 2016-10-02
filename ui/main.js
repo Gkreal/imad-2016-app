04f64547eb6af0713395895de071176a8110ae54
Gkreal <garimakhanna.k@gmail.com>
2016-10-02 13:28:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 //Move the image
+var img = document.getElementById('madi');
+var marginLeft =0;
+function moveRight (){
+    marginLeft = marginLeft+10;
+    img.style.marginLeft- marginLeft+'px';
+}
+image.onclick = function(){
+    var interval = setInterval(moveRight+100);
+}
