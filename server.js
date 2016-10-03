d19a741e9444f029a5f8d21fcbd4ba91ab423556
Gkreal <garimakhanna.k@gmail.com>
2016-10-03 17:15:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var articles = {
                     <p6><br>
                         "The key to Success is Hardwork and Determination."<br><br>
                     </p6>
-                     <img class ='img-polygon' src= "https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/269396_349956955106256_1319345635_n.jpg?oh=0ccd1c75636d1cd250b49a0e374057a3&oe=5861B1A1" style= position:centre,width:300px, height:500px;> <br><br>
+                     <img class ='img-polygon' src= "https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/269396_349956955106256_1319345635_n.jpg?oh=0ccd1c75636d1cd250b49a0e374057a3&oe=5861B1A1" style= position:centre,width:300px, height:800px;> <br><br>
                     
                     
                     Since when I started to learn, i used to be creative in my thinking and ways to expression.<br>
