9ad6a091260b756044e634aec6003dcca300e9d4
Gkreal <garimakhanna.k@gmail.com>
2016-10-30 12:06:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,8 +88,18 @@ var articles = {
                     <h2> Gesture Controlled robot </h2><br>
                     Constructed a gesture controlled robot which can be controlled by hand movements <br> with the help of a control kit that can be worn on hand like a glove using ATMEL 89C51.<br><br>
                     
+                    <h3>Rash Driving Warning System For Highway Police</h3>
+                      Constructed a system with help of 555 timer & multi-vibrator to detect the high speed of vehicles on highway. Basically a speed limit detector  for traffic police to catch high speed vehicles with a fixed specified speed limit set by the police rules.
+                      
+                    <h3>Line follower Hovercraft  </h3>
+                        A hovercraft, basically an air cushion vehicle used  for rough terrains be it in water or land. Working with the help of light detectors which follow white line on a black surface with multiple air propellers using embedded system Microcontroller 89C51.
+                    <h3> Vehicle Surveillance and Monitoring System</h3>
+                        A vehicle security device based on raspberry pi2 with intruder alert, navigation and facial recognition system. A device which is independent of the vehicle's inbuilt mechanism to provide high end security at theft prone areas and also provide help in case of emergency to the users. Moreover, nobody has time to keep check on the worker who is doing the job of cleaning a vehicle, even if we keep check, it is not possible to do that every day. So, here is a system which can not only keep check on the security of the vehicle but also act as a multipurpose tool to monitor the cleaner of the vehicle. 
+                    <h3> Data Visualization of Disaster Information Retrieval Based on Social Media </h3>
+                        Study of Data Visualization tools for disaster information retrieval via social media and find out the best suited visualization such that that information can be extracted immediately and concerned authorities may take necessary steps in case of emergency.
 
 
+                    
                     
                 </p>`
     },
