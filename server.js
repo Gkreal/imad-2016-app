2f06ec9145932646667347fdf665d268e32a83bd
Gkreal <garimakhanna.k@gmail.com>
2016-11-20 17:37:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var pool= require('pg').Pool;
+var Pool= require('pg').Pool;
 var config =  { 
     user: 'gkreal',
     database: 'gkreal',
@@ -217,7 +217,7 @@ app.get('/', function (req, res) {
  });
 
 
-var pool= new Pool (config);
+var Pool= new Pool (config);
 
 app.get('/article-db', function( req,res){
     //make a select request
