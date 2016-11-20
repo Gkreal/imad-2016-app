c0c33494532daaf2b68478a07c101f1f73430b19
Gkreal <garimakhanna.k@gmail.com>
2016-11-20 13:21:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ var articles = {
                      <img class ='img-me' src= "https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/269396_349956955106256_1319345635_n.jpg?oh=0ccd1c75636d1cd250b49a0e374057a3&oe=5861B1A1" style= position:centre,width:300px, height:800px;> <br><br>
                     
                     
+                    
                     Since when I started to learn, <br>I used to be creative in my thinking and ways of expression.<br><br>
                     I have been participating in many contests <br>and have won many awards in the field of Designing. <br><br>
                     But<br><br>
