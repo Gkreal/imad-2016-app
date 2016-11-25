926333c224230056f6126a131e571be2b40ed5f3
Gkreal <garimakhanna.k@gmail.com>
2016-11-25 12:08:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -281,12 +281,14 @@ var articles = {
                         
                       
                         
+                       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                        <style>
                         canvas {
                             border:1px solid #d3d3d3;
                             background-color: #f1f1f1;
                         }
-                        
-                        
+                        </style>
+                        </head>
                         <body onload="startGame()">
                         <script>
                         
@@ -336,27 +338,35 @@ var articles = {
                         }
                         
                         function moveup() {
-                            myGamePiece.speedY -= 1; 
+                            myGamePiece.speedY = -1; 
                         }
                         
                         function movedown() {
-                            myGamePiece.speedY += 1; 
+                            myGamePiece.speedY = 1; 
                         }
                         
                         function moveleft() {
-                            myGamePiece.speedX -= 1; 
+                            myGamePiece.speedX = -1; 
                         }
                         
                         function moveright() {
-                            myGamePiece.speedX += 1; 
+                            myGamePiece.speedX = 1; 
+                        }
+                        
+                        function clearmove() {
+                            myGamePiece.speedX = 0; 
+                            myGamePiece.speedY = 0; 
                         }
                         </script>
                         <div style="text-align:center;width:480px;">
-                          <button onclick="moveup()">UP</button><br><br>
-                          <button onclick="moveleft()">LEFT</button>
-                          <button onclick="moveright()">RIGHT</button><br><br>
-                          <button onclick="movedown()">DOWN</button>
+                          <button onmousedown="moveup()" onmouseup="clearmove()" ontouchstart="moveup()">UP</button><br><br>
+                          <button onmousedown="moveleft()" onmouseup="clearmove()" ontouchstart="moveleft()">LEFT</button>
+                          <button onmousedown="moveright()" onmouseup="clearmove()" ontouchstart="moveright()">RIGHT</button><br><br>
+                          <button onmousedown="movedown()" onmouseup="clearmove()" ontouchstart="movedown()">DOWN</button>
                         </div>
+
+
+
 
 
            
