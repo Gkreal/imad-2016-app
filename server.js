5b8056c0ebfe6972c3bb9c85b4de7307d8f586e0
Gkreal <garimakhanna.k@gmail.com>
2016-11-25 12:14:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -302,7 +302,7 @@ var articles = {
                         var myGameArea = {
                             canvas : document.createElement("canvas"),
                             start : function() {
-                                this.canvas.width = 480;
+                                this.canvas.width = 800;
                                 this.canvas.height = 270;
                                 this.context = this.canvas.getContext("2d");
                                 document.body.insertBefore(this.canvas, document.body.childNodes[0]);
