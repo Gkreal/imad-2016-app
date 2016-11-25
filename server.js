ea0d5efa158e331ab2f6b9b7c88758d172dc2f82
Gkreal <garimakhanna.k@gmail.com>
2016-11-25 12:39:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -291,6 +291,7 @@ var articles = {
                         function startGame() {
                             myGamePiece = new component(30, 30, "#FF6347", 10, 120);
                             myGameArea.start();
+                            mySound = new sound("bounce.mp3");
                         }
                         
                         var myGameArea = {
@@ -300,8 +301,14 @@ var articles = {
                                 this.canvas.height = 400;
                                 this.context = this.canvas.getContext("2d");
                                 document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+                                this.frameNo = 0;
                                 this.interval = setInterval(updateGameArea, 20);
                             },
+                             
+                             stop : function() {
+                                clearInterval(this.interval);
+                             },    
+                            
                             clear : function() {
                                 this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
                             }
@@ -322,13 +329,75 @@ var articles = {
                             this.newPos = function() {
                                 this.x += this.speedX;
                                 this.y += this.speedY;        
-                            }    
+                            } 
+                            
+                         this.crashWith = function(otherobj) {
+                            var myleft = this.x;
+                            var myright = this.x + (this.width);
+                            var mytop = this.y;
+                            var mybottom = this.y + (this.height);
+                            var otherleft = otherobj.x;
+                            var otherright = otherobj.x + (otherobj.width);
+                            var othertop = otherobj.y;
+                            var otherbottom = otherobj.y + (otherobj.height);
+                            var crash = true;
+                            if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+                                crash = false;
+                            }
+                            return crash;
+                        }
+                            
+    }
                         }
                         
                         function updateGameArea() {
-                            myGameArea.clear();
-                            myGamePiece.newPos();    
+                        var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+                        for (i = 0; i < myObstacles.length; i += 1) {
+                            if (myGamePiece.crashWith(myObstacles[i])) {
+                                mySound.play();
+                                myGameArea.stop();
+                                return;
+                            } 
+                        }
+                        myGameArea.clear();
+                            myGameArea.frameNo += 1;
+                            if (myGameArea.frameNo == 1 || everyinterval(150)) {
+                                x = myGameArea.canvas.width;
+                                minHeight = 20;
+                                maxHeight = 200;
+                                height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+                                minGap = 50;
+                                maxGap = 200;
+                                gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+                                myObstacles.push(new component(10, height, "green", x, 0));
+                                myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
+                            }
+                            for (i = 0; i < myObstacles.length; i += 1) {
+                                myObstacles[i].x -= 1;
+                                myObstacles[i].update();
+                            }
+                            myGamePiece.newPos();
                             myGamePiece.update();
+                        }
+                        
+                        function sound(src) {
+                            this.sound = document.createElement("audio");
+                            this.sound.src = src;
+                            this.sound.setAttribute("preload", "auto");
+                            this.sound.setAttribute("controls", "none");
+                            this.sound.style.display = "none";
+                            document.body.appendChild(this.sound);
+                            this.play = function(){
+                                this.sound.play();
+                            }
+                            this.stop = function(){
+                                this.sound.pause();
+                            }    
+                        }
+                        
+                        function everyinterval(n) {
+                            if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+                            return false;
                         }
                         
                         function moveup() {
@@ -354,11 +423,12 @@ var articles = {
                         </script>
                         <div style="text-align:center;width:480px;">
                           
-                <button><class = "button1" onmousedown="moveup()" onmouseup="clearmove()" ontouchstart="moveup()">UP</button>
-                <button><class = "button1" onmousedown="moveleft()" onmouseup="clearmove()" ontouchstart="moveleft()">LEFT</button>
-                <button><class = "button1" onmousedown="moveright()" onmouseup="clearmove()" ontouchstart="moveright()">RIGHT</button>
-                <button><class = "button1" onmousedown="movedown()" onmouseup="clearmove()" ontouchstart="movedown()">DOWN</button>
-                        </div>
+                          
+                        <button><class = "button1" onmousedown="moveup()" onmouseup="clearmove()" ontouchstart="moveup()">UP</button>
+                        <button><class = "button1" onmousedown="moveleft()" onmouseup="clearmove()" ontouchstart="moveleft()">LEFT</button>
+                        <button><class = "button1" onmousedown="moveright()" onmouseup="clearmove()" ontouchstart="moveright()">RIGHT</button>
+                        <button><class = "button1" onmousedown="movedown()" onmouseup="clearmove()" ontouchstart="movedown()">DOWN</button>
+                                </div>
 
 
 
