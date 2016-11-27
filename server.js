771c5a937b1193f80619dc5b44f1046b37d7fd52
Gkreal <garimakhanna.k@gmail.com>
2016-11-27 11:09:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -453,14 +453,35 @@ var articles = {
                     <div class = "container4">
                     <br><br><br><br>
                     
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Age', 'Weight'],
+          [ 8,      12],
+          [ 4,      5.5],
+          [ 11,     14],
+          [ 4,      5],
+          [ 3,      3.5],
+          [ 6.5,    7]
+        ]);
+
+        var options = {
+          title: 'Age vs. Weight comparison',
+          hAxis: {title: 'Age', minValue: 0, maxValue: 15},
+          vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
+          legend: 'none'
+        };
+
+        var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+      }
+    </script>
               
-	<!--Legends for Dataplot -->
 
-
-                        <span style="color:#FF0066"> Anand Vihar </span>  
-                        <span style="color:#9933FF"> Pitampura</span>  
-                        <span style="color:#000"> Shahadra </span>
-                        </div>
                         <br><br><br><br>
 <h4>Pie chart</h4>
 
